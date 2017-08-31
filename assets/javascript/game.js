d81d7777596542ad567c8a0fb501b4a6dce63003
kimberly owens <kimberlyowens@MacBook-Pro-2.attlocal.net>
2017-08-31 15:33:49 -0400

    crystals

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function() {
+
+    $(".img-responsive").on("click", function () {
+        var targetNumber;
+
+      //  var random = Math.floor(Math.random)() * 10 + 1;
+       var random = Math.floor(Math.random() * 20) + 1;
+
+        console.log("is this random " + random);
+
+    });
+});
+
+
+
