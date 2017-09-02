64f83cf1c4c532073b41f0628b7058f30f0f6997
kimberly owens <kimberlyowens@MacBook-Pro-2.attlocal.net>
2017-09-01 23:40:38 -0400

    raw material done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,27 @@
 
 $(document).ready(function() {
 
-   function newGame () {
-
+   // function newGame () {
+var counter = 0;
         var targetNumber;
 
       //  var random = Math.floor(Math.random)() * 10 + 1;
-       var random = Math.floor(Math.random() * 40) + 1;
+       var random = Math.floor(Math.random() * 40) + 10;
             targetNumber = random;
         console.log("is this random " + targetNumber);
-
-    };
-});
+       $(".test").text(targetNumber);
+   // };
 
 ///create four random numbers
 
-var numberOptions = "";
+var numberOptions = [];
 for (var i = 0; i < 4; i++) {
-    var randomNumbers = math.floor(math.random() * 10) + 1;
+    var randomNumbers = Math.floor(Math.random() * 10) + 1;
 
 numberOptions = randomNumbers + numberOptions;
+
+console.log("i need four numbers "+ numberOptions);
+
 }
 //down below is where we push those four random numbers into an array
 ////pseudo code \\\\
@@ -30,27 +32,43 @@ numberOptions = randomNumbers + numberOptions;
 
 for (var i = 0; i < numberOptions.length; i++) {
 
-    var imageCrystal = $(<"img">);
+    var imageCrystal = $("<img>");
 
     imageCrystal.addClass("crystal-image");
 
-    imageCrystal.attr("src","http://www.crystalclearintentions.co.nz/wp-content/uploads/crystals.jpg");
-    imageCrystal.attr("data-crystalvalue", numberOptions[i]);
 
-    $("#crystals").append(imageCrystal);
+    imageCrystal.attr("src", "http://www.crystalclearintentions.co.nz/wp-content/uploads/crystals.jpg");
+    imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+    console.log(numberOptions);
+
+    $(".test").append(imageCrystal);
+}
 
     $(".crystal-image").on("click", function (){
 
-        var crystalValue = ($(this)).attr("data-crystalValue"));
-    crystalValue = parseInt(CrystalVaule);
+        var crystalValue = ($(this).attr("data-crystalValue"));
+   crystalValue = parseInt(crystalValue);
+console.log("what is my "+crystalValue);
 
-    counter += crystalValue;
+     counter += crystalValue;
+        $(".counter").text(counter);
+
+console.log("counter "+counter);
 
 
-
+        if (counter === targetNumber) {
+            alert("You win!");
+        }
+        else if (counter >= targetNumber) {
+            alert("You lose!!");
+        }
 
 })
 
 
 
-}
+    // newGame();
+
+
+
+});
