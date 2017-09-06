e1a47c454a4eaf3d1de8b7ab115013c9d3898f4d
kimberly owens <kimberlyowens@MacBook-Pro-2.local>
2017-09-06 17:03:10 -0400

    finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,59 +1,60 @@
 
 $(document).ready(function() {
     //Setting universal variables
-    var counter = [0];
-    var wins = 1;
+    var counter = 0;
+    var wins = 0;
     var targetNumber;
-//function newGame generates the target number to beat
+    //function newGame generates the target number to beat
     function newGame () {
+        counter = 0;
         var random = Math.floor(Math.random() * 40) + 10;
         targetNumber = random;
         console.log("is this random " + targetNumber);
         $(".targetNumber").html(targetNumber);
-///function newGame create four random numbers for me to assign to the crystals
+    ///function newGame create four random numbers for me to assign to the crystals
         var numberOptions = [];
         for (var i = 0; i < 4; i++) {
             var randomNumbers = Math.floor(Math.random() * 10) + 1;
-            numberOptions = randomNumbers + numberOptions;
+            numberOptions.push(randomNumbers);
             console.log("i need four numbers " + numberOptions);
         }
-//create my for loop to assign the crystals the four random numbers I just created
+    //create my for loop to assign the crystals the four random numbers I just created
         //create new crystal images to appear in the DOM so numbers are random every time
         for (var i = 0; i < numberOptions.length; i++) {
             var imageCrystal = $("<img>");
             imageCrystal.addClass("crystal-image");
-//add the proper source attribute
+            //add the proper source attribute
             imageCrystal.attr("src", "http://www.crystalclearintentions.co.nz/wp-content/uploads/crystals.jpg");
             //assign the value that is equal to the numberOptions array value
             imageCrystal.attr("data-crystalvalue", numberOptions[i]);
             console.log(numberOptions);
-//append image to the container
+            //append image to the container
             $(".test").append(imageCrystal);
         }
     }
     //call my function
     newGame();
     //run function once user clicks on crystal
-    $(".crystal-image").on("click", function (){
-//assigns crystal value according to the user click
+    $(document).on("click", ".crystal-image", function (){
+    //assigns crystal value according to the user click
         var crystalValue = ($(this).attr("data-crystalValue"));
         //must be converted to integers
         crystalValue = parseInt(crystalValue);
         console.log("what is my "+crystalValue);
-//keep track of the value of each user click
+    //keep track of the value of each user click
         counter += crystalValue;
-        counter = parseInt(counter);
         $(".counter").html(counter);
         console.log("counter "+counter);
-//How to calculate wins
+    //How to calculate wins
         if (counter === targetNumber) {
-            alert("You win!") && wins++;
+            alert("You win!");
+            wins++;
             $(".test").empty();
             $(".counter").empty();
             newGame();
             $(".wins").text(wins);
         }
-        //how to calculate loses
+        //how to calculate losses
         else if (counter >= targetNumber) {
             alert("You lose!!");
             $(".test").empty();
